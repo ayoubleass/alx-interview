be1f5d25f8ee10c7c94d7c3711db66485522f052
ayoubleass <ayoublea@gmail.com>
2024-10-24 15:32:48 +0000

    Update: file 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,11 +6,22 @@ import sys
 import re
 
 
-def parse():
+def show_logs(status_frequency: dict, total_size: int) -> None:
     """"
     Updated regex pattern with correct groups for status code and file size
     """
-    pattern = r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[(\d{4}-\d{1,2}-\d{1,2} \d{1,2}:\d{1,2}:\d{1,2}\.\d+)\] "GET /projects/\d+ HTTP/1\.1" (\d{3}) (\d+)$'
+    print('File total_size:{}'.format(total_size))
+    for code in sorted(status_frequency):
+        print("{}: {}".format(code, status_frequency[code]))
+
+
+if __name__ == '__main__':
+    pattern = (
+        r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \['
+        r'(\d{4}-\d{1,2}-\d{1,2} \d{1,2}:\d{1,2}:\d{1,2}\.\d+)\] '
+        r'"GET /projects/\d+ HTTP/1\.1" '
+        r'(\d{3}) (\d+)$'
+    )
     status_code = [200, 301, 400, 401, 403, 404, 405, 500]
     total_size = 0
     counter = 0
@@ -26,14 +37,6 @@ def parse():
                 if code in status_code:
                     status_frequency[str(code)] += 1
                 if counter % 10 == 0:
-                    print('File total_size:{}'.format(total_size))
-                    for code in sorted(status_frequency):
-                        print("{}: {}".format(code, status_frequency[code]))
+                    show_logs(status_frequency, total_size)
     except KeyboardInterrupt:
-        print('File total_size:{}'.format(total_size))
-        for code in sorted(status_frequency):
-            print("{}: {}".format(code, status_frequency[code]))
-
-
-if __name__ == '__main__':
-    parse()
+        show_logs(status_frequency, total_size)

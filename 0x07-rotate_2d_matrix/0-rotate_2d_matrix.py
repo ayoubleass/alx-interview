7b20bc7cda98ceb52d8e3bf0d004e0340ae5b7a0
ayoubleass <ayoublea@gmail.com>
2024-11-21 13:21:44 +0000

    CREATE file 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,7 +3,6 @@
 This module has the rotate_2d_matrix function
 which rotate a matrix 90 degrees clockwise.
 """
-import copy
 
 
 def rotate_2d_matrix(matrix):
@@ -11,7 +10,10 @@ def rotate_2d_matrix(matrix):
     Rotates n x n 2D matrix 90 degrees clockwise.
     """
     n = len(matrix)
-    temp = copy.deepcopy(matrix)
-    for i in range(n):
-        for j in range(n):
-            matrix[j][i] = temp[n - i - 1][j]
+    for i in range(n // 2):
+        for j in range(i, n - i - 1):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[n - j - 1][i]
+            matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
+            matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
+            matrix[j][n - i - 1] = temp
